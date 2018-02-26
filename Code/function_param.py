9975207e8e5031249632be41980db463aaf3d9ce
kkeglje <kegljevickarlo@gmail.com>
2018-02-27 00:51:35 +0100

    more pitone davai

---
+++ Code/function_param.py
@@ -0,0 +1,15 @@
+def print_max(a,b):
+    a=a+1
+    if a>b:
+        print("a is maximum")
+    elif a==b:
+        print("a is equal to b")
+    else:
+        print("b is maximum")
+    print("In function",a)
+
+a=10
+b=10
+
+print_max(a,b)
+print("After function",a)
