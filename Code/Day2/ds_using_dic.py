41131b462a3776bec5de631417b5d53b5d401184
kkeglje <kegljevickarlo@gmail.com>
2018-02-27 20:06:37 +0100

    l

---
+++ Code/Day2/ds_using_dic.py
@@ -0,0 +1,20 @@
+ab={
+    'Karlo':'kegljevickarlo@gmail.com',
+    'jedan':'jedanmail',
+    'nisa':'nisan',
+    'spam':'saldasd@ls'
+}
+
+print("Karlo adress is",ab['Karlo'])
+
+del ab['spam']
+
+print("There are {} contats in my dictionary".format(len(ab)))
+
+for name,adress in ab.items():
+    print("Contact {} at {}".format(name,adress))
+
+ab['Guido']='guide@gmail.com'
+
+if 'Guido' in ab:
+    print('\n Guido adress is',ab['Guido'])
