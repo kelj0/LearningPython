0f386dd66215648f4b8bc79bf6f658958b00c92e
kkeglje <kegljevickarlo@gmail.com>
2018-03-01 11:43:02 +0100

    backup_V2

--- Code/Day4/backup_ver2.py
+++ Code/Day4/backup_ver2.py
@@ -1,6 +1,28 @@
-def string_splosion(str):
-    counter=1
-    for i in str:
-        print(str[0:counter],end='')
-        counter+=1
-string_splosion('Code')
+import os
+import time
+
+source=['"D:\\--python\\Day 4"']
+
+targer_dir='D:\\--python\\backup2'
+
+if not os.path.exists(targer_dir):
+    os.mkdir(targer_dir)
+
+today=targer_dir+os.sep+time.strftime('%Y%d%m')
+now=time.strftime('%H%M%S')
+
+targer=today+os.sep+now+'.zip'
+
+if not os.path.exists(today):
+    os.mkdir(today)
+    print('Successfully creaded directory',today)
+
+zip_command='zip -r {0} {1}'.format(targer,' '.join(source))
+
+print('Zip command is:')
+print(zip_command)
+print('Running')
+if os.system(zip_command)==0:
+    print('Successful backup to',targer)
+else:
+    print('Backup FAILED')
