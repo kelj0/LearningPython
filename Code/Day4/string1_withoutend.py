062b8b312dbfa84a0b4f575ea3cc3d68d2b5d136
kkeglje <kegljevickarlo@gmail.com>
2018-03-02 01:06:32 +0100

    string1_withoutend

---
+++ Code/Day4/string1_withoutend.py
@@ -0,0 +1,12 @@
+#Given a string, return a version without the first and last char, 
+# so "Hello" yields "ell". The string length will be at least 2.
+
+
+def without_end(str):
+    a=len(str)-1
+    a=int(a)
+    return str[1:a]
+
+without_end('Hello') 
+without_end('java')
+without_end('coding')
