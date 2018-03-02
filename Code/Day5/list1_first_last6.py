7b5f8d669ac62cdcd6b69c478c8cb1fd0ec0e1c2
kkeglje <kegljevickarlo@gmail.com>
2018-03-03 00:51:59 +0100

    a

--- Code/Day5/list1_first_last6.py
+++ Code/Day5/list1_first_last6.py
@@ -1,6 +1,6 @@
 #Given an array of ints, return True if 6 appears as either 
-# the first or last element in the array.
-#  The array will be length 1 or more.
+#the first or last element in the array.
+#The array will be length 1 or more.
 def first_last6(nums):
   a=len(nums)-1
   a=int(a)
@@ -8,7 +8,6 @@ def first_last6(nums):
     return True
   else:
     return False
-
 print(first_last6([1, 2, 6]))
 print(first_last6([6, 1, 2, 3]))
 print(first_last6([13, 6, 1, 2, 3]))
