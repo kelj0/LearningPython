1ea2aaeeb42f291cd99dc99569cbbb53a115f69e
kkeglje <kegljevickarlo@gmail.com>
2018-03-06 00:02:36 +0100

    mul_table V2

--- Code/Day8/mult_table.py
+++ Code/Day8/mult_table.py
@@ -1,4 +1,7 @@
 for i in range(11):
     for j in range(11):
-        print(str(i*j)+"|",end="")
+        if (i*j)>=10:
+            print(str(i*j)+"|",end="")
+        else:
+            print(str(i*j)+" |",end="")
     print()
