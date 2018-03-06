f5b01668e56f04e0673a32853431a3c21d781d2e
kkeglje <kegljevickarlo@gmail.com>
2018-03-06 21:12:05 +0100

    practicing_dictmore

---
+++ Code/Week2/Day9/practicing_dict.py
@@ -0,0 +1,5 @@
+names={"imena":["karlo","pero","duro"],"prezimena":["kegljevic","peric","duric"]}
+
+for i in range(3):
+    print("IME:",names['imena'][i])
+    print("PREZIME:",names['prezimena'][i])
