ad4f239104451c3195bb38baf3f365ea5ba597e3
kkeglje <kegljevickarlo@gmail.com>
2018-03-09 18:56:43 +0100

    remastering isPgoneNumber with regex

---
+++ Code/Week2/Day12/isPhoneNumberRegex.py
@@ -0,0 +1,11 @@
+import re
+
+def isPhoneNumber(text):
+    check= re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+    mo= check.search(text)
+    print("TESTS:",mo,mo.group(),str(mo.group()),sep="|||")
+    if len(str(mo.group))>1:
+        return mo.group()
+    else:
+        return "Number not found"
+print('Number is: '+ isPhoneNumber("Tel number is 123-123-1234"))
