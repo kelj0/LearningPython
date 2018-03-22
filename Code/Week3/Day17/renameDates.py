0e50e041ddef2fefca45b2560e7023e3646bf45b
kkeglje <kegljevickarlo@gmail.com>
2018-03-23 00:45:44 +0100

    Finally done with renameDates.py

---
+++ Code/Week3/Day17/renameDates.py
@@ -0,0 +1,32 @@
+# renameDates.py - Renames filenames with American MM-DD-YYYY date format
+# to European DD-MM-YYYY
+
+import os,shutil,re
+
+datePattern = re.compile(r"""^(.*?)
+    ((0|1)?\d)-
+    ((0|1|2|3)?\d)-
+    ((19|20)\d\d)
+    (.*?)$
+""",re.VERBOSE)
+
+
+for amerFilename in os.listdir('.'):
+    mo = datePattern.search(amerFilename)
+    if mo==None:
+        continue
+
+    beforePart = mo.group(1)
+    monthPart = mo.group(2)
+    dayPart = mo.group(4)
+    yearPart = mo.group(6)
+    afterPart = mo.group(8)
+
+euroFileName = beforePart+dayPart+'-'+monthPart+'-'+ yearPart+'-'+afterPart
+
+absWorkingDir = os.path.abspath('.')
+amerFilename = os.path.join(absWorkingDir,amerFilename)
+euroFileName = os.path.join(absWorkingDir,euroFileName)
+
+print('Renaming "%s" to "%s"...'%(amerFilename,euroFileName))
+shutil.move(amerFilename,euroFileName)
