75d7bcc623808637c00ae0cf993424e73621c3a4
kkeglje <kegljevickarlo@gmail.com>
2018-03-23 23:46:08 +0100

    backup folder to zipfile_x.zip (x increments on every backup of same folder

---
+++ Code/Week3/Day18/backupToZip.py
@@ -0,0 +1,29 @@
+# backupToZip.py - Copies an entire folder and its contents into a zip
+# whose filename increments
+
+import zipfile,os
+
+def backupToZip(folder):
+    folder = os.path.abspath(folder)
+    number = 1
+    while True:
+        zipFilename = os.path.basename(folder)+'_'+str(number)+'.zip'
+        if not os.path.exists(zipFilename):
+            break
+        number = number+1
+    print('Creating %s...' %(zipFilename))
+    backupZip = zipfile.ZipFile(zipFilename,'w')
+
+    for folderName,subfilename,filenames in os.walk(folder):
+        print('Adding files in %s...'%(folderName))
+        backupZip.write(folderName)
+        for filename in filenames:
+            newBase / os.path.basename(folder)+'_'
+            if filename.startswith(newBase) and filename.startswith('.zip'):
+                continue
+            backupZip.write(os.path.join(folderName,filename))
+    backupZip.close()
+    print('Done')
+
+
+backupToZip("C:\\Users\\Karlo\\Desktop\\cats")
