d5d6a79248a4a0aca25b6b36132eb34b9a8b9ce3
kkeglje <kegljevickarlo@gmail.com>
2018-04-01 14:07:15 +0200

    Basic prototype of script playing 2048 game.. still need to fix try catch

---
+++ Code/Week4/Day23/2048.py
@@ -0,0 +1,28 @@
+# 2048.py script automaticly plays game 2048
+
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+
+browser.get('https://gabrielecirulli.github.io/2048/')
+
+linkKey = browser.find_element_by_tag_name('html')
+
+while True:
+    linkKey.send_keys(Keys.ARROW_DOWN)
+    browser.implicitly_wait(0.1)
+    linkKey.send_keys(Keys.ARROW_LEFT)
+    browser.implicitly_wait(0.1)
+    linkKey.send_keys(Keys.ARROW_UP)
+    browser.implicitly_wait(0.1)
+    linkKey.send_keys(Keys.ARROW_RIGHT)
+    browser.implicitly_wait(0.1)
+    # try:
+    #     gameOver = browser.find_element_by_class_name('game-message game-over')
+    # except Exception:
+    #     print('Still playing..')
+    #     continue
+    #retry = browser.find_element_by_class_name('retry-button')
+    #retry.click()
