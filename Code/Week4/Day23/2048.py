540e1f6c1be4c2afeb5d2087f1a4bff705a8cbbf
kkeglje <kegljevickarlo@gmail.com>
2018-04-01 15:03:54 +0200

    added try except , game will restart after game over

--- Code/Week4/Day23/2048.py
+++ Code/Week4/Day23/2048.py
@@ -9,20 +9,24 @@ browser = webdriver.Firefox()
 browser.get('https://gabrielecirulli.github.io/2048/')
 
 linkKey = browser.find_element_by_tag_name('html')
-
+browser.close()
+retry = browser.find_element_by_class_name('retry-button')
 while True:
     linkKey.send_keys(Keys.ARROW_DOWN)
-    browser.implicitly_wait(0.1)
+    browser.implicitly_wait(1)
     linkKey.send_keys(Keys.ARROW_LEFT)
-    browser.implicitly_wait(0.1)
+    browser.implicitly_wait(1)
     linkKey.send_keys(Keys.ARROW_UP)
-    browser.implicitly_wait(0.1)
+    browser.implicitly_wait(1)
     linkKey.send_keys(Keys.ARROW_RIGHT)
-    browser.implicitly_wait(0.1)
-    # try:
-    #     gameOver = browser.find_element_by_class_name('game-message game-over')
-    # except Exception:
-    #     print('Still playing..')
-    #     continue
-    #retry = browser.find_element_by_class_name('retry-button')
-    #retry.click()
+    browser.implicitly_wait(1)
+    try:
+        retry.click()
+    except Exception:
+        print('Playing...')
+        
+
+
+
+    
+    
