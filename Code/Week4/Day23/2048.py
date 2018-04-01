02f741ea23cc8c72d73509e5a054d684670933d5
kkeglje <kegljevickarlo@gmail.com>
2018-04-01 15:46:06 +0200

    Over with restart game and sleep 10s after script lose so you can look score

--- Code/Week4/Day23/2048.py
+++ Code/Week4/Day23/2048.py
@@ -1,28 +1,35 @@
 # 2048.py script automaticly plays game 2048
 
-
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+import time
+
 
 browser = webdriver.Firefox()
 
 browser.get('https://gabrielecirulli.github.io/2048/')
 
 linkKey = browser.find_element_by_tag_name('html')
-browser.close()
+
 retry = browser.find_element_by_class_name('retry-button')
 while True:
+    
     linkKey.send_keys(Keys.ARROW_DOWN)
-    browser.implicitly_wait(1)
+    time.sleep(0.01)
     linkKey.send_keys(Keys.ARROW_LEFT)
-    browser.implicitly_wait(1)
+    time.sleep(0.01)
     linkKey.send_keys(Keys.ARROW_UP)
-    browser.implicitly_wait(1)
+    time.sleep(0.05)
     linkKey.send_keys(Keys.ARROW_RIGHT)
-    browser.implicitly_wait(1)
+    time.sleep(0.05)
+    
     try:
-        retry.click()
-    except Exception:
+        gameOver = browser.find_element_by_class_name('game-over')
+        if gameOver:
+            time.sleep(10)
+            retry.click()
+    except NoSuchElementException:
         print('Playing...')
         
 
