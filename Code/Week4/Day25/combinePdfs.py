765c3182e979a012989e18411678c82bddc9f4f0
kkeglje <kegljevickarlo@gmail.com>
2018-04-04 21:17:12 +0200

    combinePdfs.py-pip install PyPDF2 if not installed

---
+++ Code/Week4/Day25/combinePdfs.py
@@ -0,0 +1,22 @@
+# combinePdfs.py - Combines all the PDFs in the current working directory
+# into a single PDF
+
+import PyPDF2,os
+
+pdfFiles = []
+for filename in os.listdir('.'):  #'.'-returns ever file in cur. dir.
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort(key=str.lower)
+
+pdfWriter = PyPDF2.PdfFileWriter()
+for filename in pdfFiles:
+    pdfFileObj = open(filename,'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    for pageNum in range(1,pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+pdfOutput = open('CombinedPDFfiles.pdf','wb')
+pdfWriter.write(pdfOutput)
+pdfOutput.close()
