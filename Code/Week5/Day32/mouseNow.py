8150eb0deb492d7018b730c672005cd9f677aa65
kkeglje <kegljevickarlo@gmail.com>
2018-04-11 21:40:40 +0200

    added script to show position of mouse

---
+++ Code/Week5/Day32/mouseNow.py
@@ -0,0 +1,15 @@
+# mouseNow.py - Prints current position of mouse
+
+import pyautogui,time,os
+
+try:
+    print('Press Ctrl+C to quit(in console)')
+    while 1:
+        W,H=pyautogui.position()
+        posStr = '('+str(W)+', '+str(H)+')'
+        print(posStr,end='')
+        time.sleep(0.2)
+        print('\b'*len(posStr),end='',flush=True)
+except KeyboardInterrupt:
+    print()
+    print('Ty for using my script')
