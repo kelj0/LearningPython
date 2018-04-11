63ba790ea56e47db12d218d72e543393506288ed
kkeglje <kegljevickarlo@gmail.com>
2018-04-11 22:46:36 +0200

    spiral drawing scrip

---
+++ Code/Week5/Day32/spiralDraw.py
@@ -0,0 +1,17 @@
+import pyautogui,time
+
+i=5
+while i>0:
+    print('Drawing starts in %ss' %(i))
+    time.sleep(1)
+    i-=1
+
+pyautogui.click()
+distance = 200
+while distance>0:
+    pyautogui.dragRel(distance,0)
+    distance-=5
+    pyautogui.dragRel(0,distance)
+    pyautogui.dragRel(-distance,0)
+    distance-=5
+    pyautogui.dragRel(0,-distance)
