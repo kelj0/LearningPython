ae11e6682bf57dc93fe3a41b895f30cc5c92de6b
kkeglje <kegljevickarlo@gmail.com>
2018-05-05 13:41:54 +0200

    Playing with turtle module :P

---
+++ Code/Week6/Day39/turtleMovement.py
@@ -0,0 +1,25 @@
+import turtle
+import msvcrt
+
+screen = turtle.Screen()
+
+left = b'K'
+right = b'M'
+up = b'H'
+down = b'P'
+esc = b'\x1b'
+player = turtle.Turtle()
+player.shape("turtle")
+
+speed = 10
+while msvcrt.getch()!=esc:
+    player.forward(speed)
+    if msvcrt.getch()==left:
+        player.left(10)
+    elif msvcrt.getch()==right:
+        player.right(10)
+    
+    if msvcrt.getch()==up:
+        speed+=5
+    elif msvcrt.getch()==down:
+        speed-=5
