d1b0f3878d540292d289a0cc077fc0898761f52a
kkeglje <kegljevickarlo@gmail.com>
2018-05-08 20:48:12 +0200

    extract emails from file

---
+++ Code/Projects/extractEmailsFromFile.py
@@ -0,0 +1,15 @@
+# Extract emails from txt file , just replate file.txt to yourfilename.txt :)
+# Also if you want to write it to file replace print(e.group())[line 12] to something like file.write...
+import re
+
+emails = open("file.txt","r")
+emailList = []
+emails.readline()
+for line in emails:
+    emailList.append(re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}\b',line))
+for e in emailList:
+    try:
+        print(e.group())
+    except AttributeError:
+        continue
+emails.close()
