8a477fab974b093324b122a0de6b4bf6e0f9d3ca
Keljo <keljo@keljo.lan>
2018-05-14 23:59:12 +0200

    guessmynumber from thinkcspy

---
+++ Code/CodesFromBooks/Week6/Day41/guessMyNumber.py
@@ -0,0 +1,17 @@
+import random
+
+rnd = random.Random()
+number = rnd.randint(1,1000)
+guesses = 0
+msg = ""
+
+while True:
+    guess = int(input(msg+"\nGuess my number between 1 and 1000: "))
+    guesses +=1
+    if guess>number:
+        msg+=str(guess) + ' is too high.\n'
+    elif guess<number:
+        msg+=str(guess)+ ' is too low.\n'
+    else:
+        break
+print("Nois, you guessed it in {} guesses".format(guesses))
