867c9ca8703c07dc42963fb734f6eb66671d3601
Karlo Kegljevic <kegljevickarlo@gmail.com>
2018-06-27 11:49:35 +0200

    Update ColorDecoder.py

--- Code/RandomStuff/ColorDecoder.py
+++ Code/RandomStuff/ColorDecoder.py
@@ -6,7 +6,13 @@ colorFile = open("colorsT.txt",'w')
 counter = 0
 for y in range(1,129,16):
     for x in range(0,255,16):
-        colorFile.write("\ncase " + str(counter)+":"+"\n\t"+"pixels[i] = "+str(pix[x,y][0])+";   //r"+"\n\t"+"pixels[i+1] = "+str(pix[x,y][1])+";   //g"+"\n\t"+"pixels[i+2] = "+str(pix[x,y][2])+";   //b"+"\n\t"+"pixels[i+3] = "+str(pix[x,y][3])+";   //a"+"\n\tbreak;")
+        colorFile.write("""case {0}:
+            pixels[i] = {1};    //r
+            pixels[i+1] = {2};   //g
+            pixels[i+2] = {3};   //b
+            pixels[i+3] = {4};   //a
+            break;
+            """.format(str(counter),str(pix[x,y][0]),str(pix[x,y][1]),str(pix[x,y][2]),str(pix[x,y][3])))
         counter+=1
     colorFile.write("\n ")
     
