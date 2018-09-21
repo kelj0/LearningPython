7df1b82a505c111301209a8c51715129efc65e70
kkeglje <kegljevickarlo@gmail.com>
2018-09-21 12:58:52 +0200

    basic stuff

---
+++ Code/CodesFromBooks/OnlineChallenges/aababcaba.py
@@ -0,0 +1,14 @@
+
+word = "test"
+l = [word[0]]
+
+reverse = False
+
+while len(l) != 0:
+    print(''.join(l))
+    if len(l) == len(word):
+        reverse = True
+    if reverse:
+        l.pop()
+    else:
+        l.append(word[len(l)])
