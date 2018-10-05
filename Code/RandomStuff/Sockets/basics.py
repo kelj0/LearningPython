d844fb6f6c39906ff90e645224a753bd082ada22
kkeglje <kegljevickarlo@gmail.com>
2018-10-05 14:48:17 +0200

    added basic chatServer

--- Code/RandomStuff/Sockets/basics.py
+++ Code/RandomStuff/Sockets/basics.py
@@ -1,21 +1,31 @@
 import socket
-from _thread import *
+from _thread import start_new_thread
+import time
+import os,sys
 
 HOST = ''
 PORT = 5000
 
-def clientThread(conn):
-    conn.send(str.encode("Welcome to the server. Type something and hit enter\n"))
+def clientThread(conn,addr):
     while True:
-        data = conn.recv(2048)
-        reply = 'Server output: ' + data.decode('utf-8')
-        if not data:
-            break
-        conn.sendall(str.encode(reply))
-    conn.close()
+        try:
+            data = conn.recv(4096)
+            if not data:
+                break
+            conn.send(str.encode('HTTP/1.0 200 OK\n'))
+            conn.send(str.encode('Content-Type: text/html\n\n'))
+            conn.send(str.encode(''.join(open('hello.html','r').readlines())))
+        except OSError:
+            continue
+        print("Response sent to client")
+        conn.close()
+        
+
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
     print("Socket created")
     try:
         s.bind((HOST,PORT))
@@ -23,13 +33,14 @@ def main():
         print(str(e))
     print("Socket bind complete")
 
+    
     s.listen(5)
     print("Socket now listening")
 
     while True:
         conn, addr = s.accept()
         print("Connected to {0}:{1}".format(addr[0],addr[1]))
-        start_new_thread(clientThread,(conn,))
+        start_new_thread(clientThread,(conn,addr,))
     s.close()
 
 
