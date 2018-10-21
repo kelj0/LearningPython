c97b211655cdc4585220dd79814f93a411f19d2b
kelj0 <kegljevickarlo@gmail.com>
2018-10-21 13:08:08 +0200

    indexscraper

---
+++ Code/UsefulScripts/indexScraper.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "hyyp://index.page.com/index"
+req = requests.get(URL)
+soup = BeautifulSoup(req.text,"html.parser")
+PATH = "/path/to/download/folder"
+for link in soup.find_all('a')[8:]:
+    songUrl = URL+link['href']
+    songName = link['href'].replace('%','_')
+    r = requests.get(songUrl,stream=True)
+    print("Downloading "+songName)
+    with open(PATH+songName,'wb') as f:
+        for chunk in r.iter_content(chunk_size=4096): 
+            if chunk:
+                f.write(chunk)
+    print("Done!")
