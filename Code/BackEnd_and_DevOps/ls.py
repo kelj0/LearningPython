ffc224d85e6e985d8d632abfe955d3f3a3b37628
kelj0 <kegljevickarlo@gmail.com>
2018-12-13 23:14:29 +0100

    added -a to ls

--- Code/BackEnd_and_DevOps/ls.py
+++ Code/BackEnd_and_DevOps/ls.py
@@ -1,21 +1,24 @@
 #! /usr/bin/python3
 
-import sys,os
-
+import sys,os,argparse
 
 def noArg():
-    return '\n'.join([f for f in os.listdir(os.getcwd())])
+    return '\n'.join(sorted([f for f in os.listdir(os.getcwd()) if not f.startswith('.')]))
 
-def help():
-    return "Wrong args!\nUsage:\n-te\n-tes\n-test"""
+def listAll():
+    return '\n'.join(sorted([f for f in os.listdir(os.getcwd())]))
 
 def main():
+    parser = argparse.ArgumentParser(description="ls like command implemented in python")
+    parser.add_argument('-a',help="Lists all files in current dir.",dest="listAll",action="store_true")
     if len(sys.argv) == 1:
         print(noArg())
     else:
-        print(help())
-    
-    
+        parsed = parser.parse_args()
+        if parsed.listAll:
+            print(listAll())
+
+
 if __name__ == '__main__':
     main()
 
