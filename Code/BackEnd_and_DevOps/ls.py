92ac07d6f403562f1a48fc7fb6ed678b181b3d28
kelj0 <kegljevickarlo@gmail.com>
2018-12-14 22:46:16 +0100

    added colors

--- Code/BackEnd_and_DevOps/ls.py
+++ Code/BackEnd_and_DevOps/ls.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python3
 
 import sys,os,argparse
+from termcolor import colored
 
 def noArg():
     return '\n'.join(sorted([f for f in os.listdir(os.getcwd()) if not f.startswith('.')]))
@@ -8,15 +9,23 @@ def noArg():
 def listAll():
     return '\n'.join(sorted([f for f in os.listdir(os.getcwd())]))
 
+def listColor():
+    return '\n'.join([colored(f,'cyan') if os.path.isdir(f) else f for f in os.listdir(os.getcwd())])
+    
+
 def main():
     parser = argparse.ArgumentParser(description="ls like command implemented in python")
     parser.add_argument('-a',help="Lists all files in current dir.",dest="listAll",action="store_true")
+    parser.add_argument('--color',help="Lists all files in current dir.(colored)",dest="listColor",action="store_true")
+
     if len(sys.argv) == 1:
         print(noArg())
     else:
         parsed = parser.parse_args()
         if parsed.listAll:
             print(listAll())
+        elif parsed.listColor:
+            print(listColor())
 
 
 if __name__ == '__main__':
