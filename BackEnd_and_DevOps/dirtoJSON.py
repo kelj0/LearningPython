a6782bb510a88d5c1a02bead70de7b700e17ee5d
kelj0 <kegljevickarlo@gmail.com>
2018-12-18 18:21:13 +0100

    dirtojson

--- BackEnd_and_DevOps/dirtoJSON.py
+++ BackEnd_and_DevOps/dirtoJSON.py
@@ -1,26 +1,20 @@
-import sys, os, json, pprint
+import os, json
 
-_json = {}
 
-def getFiles(path,fold="./"):
-    global _json
-    for f in os.listdir(path):
-        if os.path.isdir(f):
-            _json[f] = []
-            print("Found new dir: " + f)
-            getFiles(path+'/'+f,f)
-        else:
-            print("Appending files in directory: " + fold)
-            if fold in _json:
-                _json[fold].append(f)
-            else:
-                _json[fold] = [f]
-                
+def path_to_dict(path):
+    d = {'name': os.path.basename(path)}
+    if os.path.isdir(path):
+        d['type'] = "directory"
+        d['children'] = [path_to_dict(os.path.join(path,x)) for x in os.listdir(path)]
+    else:
+        d['type'] = "file"
+    return d
+
 def main():
-    getFiles(os.getcwd())
-    pprint.pprint(_json)
+    print(json.dumps(path_to_dict('.'),indent=2,sort_keys=True))
 
 
 if __name__=='__main__':
+
     main()
 
