165cac2105132dd23c2ac7085eef91bddd18bddf
kelj0 <kegljevickarlo@gmail.com>
2018-12-24 23:40:24 +0100

    todo:save css and js from sites

--- BackEnd_and_DevOps/backupSite/backupSite.py
+++ BackEnd_and_DevOps/backupSite/backupSite.py
@@ -1,24 +1,41 @@
-import requests,sys,re, os,shutil
+import requests,sys,re,os,shutil
 from bs4 import BeautifulSoup as bs
 
 
+srcPattern = re.compile('(?<=src=(\'|\"))(.*?)(?=\s*(\'|\"))')
+hrefPattern = re.compile('(?<=href=(\'|\"))(.*?)(?=\s*(\'|\"))')
+
 def saveImages(BSoup,url):
-    for p in BSoup.findAll('img'):
-        print("Downloading {}...".format(p['src'].split('/')[-1]))
-        p['src'] = p['src'][1:] #remove . that i put into plain html
-        os.makedirs("site"+'/'.join(p['src'].split('/')[:-1]))
-        with open("site"+p['src'],'wb') as f:
-            for chunk in requests.get(url+p['src'],stream=True).iter_content(2048):
+    for p in BSoup.findAll('img')[:4]:
+        print("[Downloading image] {}...".format(p['src'].split('/')[-1]))
+        with open("site/images/"+p['src'].split('/')[-1],'wb') as f:
+            for chunk in requests.get(p['src'],stream=True).iter_content(1024):
                 f.write(chunk)
 
-def saveLinks(BSoup):
-    for l in BSoup.findAll('a'):
-        print(l)
-        # with open("site/")
-        #     requests.get(l['href']).text.replace('src="','src=".')
+def downloadHtml(link,name):
+    r = requests.get(link).text
+    saveImages(bs(r,'html.parser'),link)
+    src = srcPattern.findall(r)
+    href = hrefPattern.findall(r)
+    for s in src:
+        print("Adjusting src: " + s[1].split('/')[-1])
+        if not (s[1][-4:-1]=='css' or s[1][-3:-1] == 'js'):
+            r = r.replace(s[1],'images/'+s[1].split('/')[-1]+'"',1)
+        elif s[1][-4:-1]=='css':
+            r = r.replace(s[1],'style/'+s[1].split('/')[-1]+'"',1)
+        else:
+            r = r.replace(s[1],'script/'+s[1].split('/')[-1]+'"',1)
+    for s in href:
+        print("Adjusting href: " + s[1])
+        if not (s[1][-4:-1]=='css' or s[1][-3:-1] == 'js'):
+            r = r.replace(s[1],'images/'+s[1].split('/')[-1]+'"',1)
+        elif s[1][-4:-1]=='css':
+            r = r.replace(s[1],'style/'+s[1].split('/')[-1]+'"',1)
+        else:
+            r = r.replace(s[1],'script/'+s[1].split('/')[-1]+'"',1)
 
-
-
+    with open("site/"+name+'.html',"w") as f:
+        f.write(r)
 
 
 def main():
@@ -35,29 +52,30 @@ def main():
 
     try:
         os.mkdir("site")
+        os.mkdir("site/images")
+        os.mkdir("site/style")
+        os.mkdir("site/script")
     except FileExistsError:
         print("Folder 'site' is already in directory. Do you want to delete it?")
-        if str(input("(y/n)\n")).lower()!='y':
+        if str(input("(y/n) ")).lower()!='y':
             return
         print("Deleting folder 'site'!...")
         shutil.rmtree("./site")
         print("Making new folder site")
         os.mkdir("site")
-
-    r = requests.get(ar[1])
-    sr = r.text
-    sr = sr.replace('src="','src=".')
-    with open("site/index.html","w") as f:
-        f.write(sr)
-    b = bs(sr,'html.parser')
-
-    saveImages(b,ar[1])
-    #TODO:
-    # saveLinks(b)
-
-
-
-
+        os.mkdir("site/images")
+        os.mkdir("site/style")
+        os.mkdir("site/script")
+    
+    downloadHtml(ar[1],'index')
+    for link in bs(requests.get(ar[1]).text,'html.parser').findAll('a'):
+        try:
+            print("---------------------\nFound new link! [{}] downloading html..\n---------------------".format(link['href']))
+            downloadHtml(link['href'],link['href'].split('/')[-1])
+        except requests.exceptions.MissingSchema:
+            downloadHtml(ar[1]+link['href'],link['href'].split('/')[-1])
+        except KeyError:
+            print(link)
 
 
 if __name__ == "__main__":
