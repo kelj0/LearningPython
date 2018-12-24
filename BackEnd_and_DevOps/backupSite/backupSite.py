7716479d8d5cc31c7da1534b746480a55cf521b5
kelj0 <kegljevickarlo@gmail.com>
2018-12-24 01:30:09 +0100

    added img downloading and local relinking

--- BackEnd_and_DevOps/backupSite/backupSite.py
+++ BackEnd_and_DevOps/backupSite/backupSite.py
@@ -1,5 +1,23 @@
-import requests,sys,re
+import requests,sys,re, os,shutil
 from bs4 import BeautifulSoup as bs
+
+
+def saveImages(BSoup,url):
+    for p in BSoup.findAll('img'):
+        print("Downloading {}...".format(p['src'].split('/')[-1]))
+        p['src'] = p['src'][1:] #remove . that i put into plain html
+        os.makedirs("site"+'/'.join(p['src'].split('/')[:-1]))
+        with open("site"+p['src'],'wb') as f:
+            for chunk in requests.get(url+p['src'],stream=True).iter_content(2048):
+                f.write(chunk)
+
+def saveLinks(BSoup):
+    for l in BSoup.findAll('a'):
+        print(l)
+        # with open("site/")
+        #     requests.get(l['href']).text.replace('src="','src=".')
+
+
 
 
 
@@ -14,11 +32,29 @@ def main():
         return
     if not ar[1].startswith("http"):
         ar[1] = "http://"+ar[1]
+
+    try:
+        os.mkdir("site")
+    except FileExistsError:
+        print("Folder 'site' is already in directory. Do you want to delete it?")
+        if str(input("(y/n)\n")).lower()!='y':
+            return
+        print("Deleting folder 'site'!...")
+        shutil.rmtree("./site")
+        print("Making new folder site")
+        os.mkdir("site")
+
     r = requests.get(ar[1])
-    with open("test.html","w") as f:
-        f.write(r.text)
-    #b = bs(r.text,'html.parser')
-    #TODO: download pictures, and all sites linked on site
+    sr = r.text
+    sr = sr.replace('src="','src=".')
+    with open("site/index.html","w") as f:
+        f.write(sr)
+    b = bs(sr,'html.parser')
+
+    saveImages(b,ar[1])
+    #TODO:
+    # saveLinks(b)
+
 
 
 
