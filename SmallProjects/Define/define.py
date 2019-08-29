2563088bf44f4d5e7f7d65f3c41f12fdaef4a1e4
kelj0 <kegljevickarlo@gmail.com>
2019-08-29 17:31:36 +0200

    fix command injection

--- SmallProjects/Define/define.py
+++ SmallProjects/Define/define.py
@@ -22,10 +22,10 @@ def main():
             try:    
                 data = json.loads(req.text)['data']['content'][0]['entries'][0]['posBlocks'][0]['definitions']
             except TypeError:
-                os.system('notify-send "Cant find |%s| on dictionary.com!"' % word)
+                os.system('notify-send "Cant find that word on dictionary.com!"')
                 wordChc = True
             except KeyError:
-                os.system('notify-send "Cant find |%s| on dictionary.com!"' % word)
+                os.system('notify-send "Cant find that word on dictionary.com!"')
                 wordChc = True
 
             if not wordChc:
@@ -34,17 +34,16 @@ def main():
                     for definition in data[:3]:
                         definitions.append(cleanhtml(definition['definition']))
                         definitions.append("------------")
-                    os.system('notify-send "definitions from dictionary.com:[{}\n{}"'\
-                    .format(word+"]\n------------",'\n'.join(definitions)))
+                    os.system('notify-send "definitions from dictionary.com:\n{}"'.format('\n'.join(definitions)))
                 except KeyError:
                     os.system('notify-send "no results in dictionary.com"')
             try:    
                 dataURB = json.loads(reqURB.text)['list']
             except TypeError:
-                os.system('notify-send "Cant find |%s| on urbandictionary.com!"' % word)
+                os.system('notify-send "Cant find that word on urbandictionary.com!"' % word)
                 wordChcURB = True
             except KeyError:
-                os.system('notify-send "Cant find |%s| on urbandictionary.com!"' % word)
+                os.system('notify-send "Cant find that word on urbandictionary.com!"' % word)
                 wordChcURB = True
 
             if not wordChcURB:    
@@ -52,8 +51,7 @@ def main():
                 for definition in dataURB[:3]:
                     definitionsURB.append(definition['definition'])
                     definitionsURB.append("------------")
-                os.system('notify-send "definitions from urbandictionary.com:[{}\n{}"'\
-                .format(word+"]\n------------",'\n'.join(definitionsURB)))
+                os.system('notify-send "definitions from urbandictionary.com:\n{}"'.format('\n'.join(definitionsURB)))
     os.system('notify-send "Thank you for using define.py made by kelj0"')
 
 
